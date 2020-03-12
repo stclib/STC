c2700ca50eca6b0211073079f8c1048ed9b88dfb
Tyge Løvset <60263450+tylo-work@users.noreply.github.com>
2020-03-12 19:57:33 +0100

    Add files via upload

--- benchmark.cpp
+++ benchmark.cpp
@@ -11,84 +11,59 @@ declare_CVector_string(s);
 
 int main()
 {
-    CMap_ii map = cmap_initializer;
-    int K = 300000;
-    printf("BEGIN\n");
-    //cmap_ii_reserve(&map, 50);
-    /*
-    cmap_ii_setMaxLoadFactor(&map, 0.8);
-    for (size_t i = 0; i < K; ++i) {
-        cmap_ii_put(&map, i*i, i);
-    }
-    for (size_t i = 0; i < K/2; ++i) {
-        cmap_ii_erase(&map, i*i);
-    }
-    c_foreach (i, cmap_ii, map) cmap_ii_bucket(map, i.item->key);
-    for (size_t i = 0; i < K/2; ++i) {
-        cmap_ii_put(&map, i, i*i);
-    }    
-    c_foreach (i, cmap_ii, map) cmap_ii_bucket(map, i.item->key);
-    */
-    
-    uint64_t checksum = 0;
     clock_t before, difference;
-    size_t fib1, fib2, fibx;
-    
-    const size_t N = 20000000;
+    CMap_ii map = cmap_initializer;
+
+    uint64_t checksum = 0, erased, get;
+    const size_t N = 50000000;
 
     printf("Starting %llu\n", N);
     //cmap_ii_reserve(&map, N * 1.25);
+    cmap_ii_clear(&map);
+    srand(123);
     before = clock();
-    fib1 = 0, fib2 = 1; checksum = 0;
+    checksum = 0; erased = 0; get = 0;
     for (size_t i = 0; i < N; ++i) {
-        checksum += ++cmap_ii_put(&map, FIBONACCI_NEXT, i)->value;
+		int rnd = rand();
+		int op = rand() >> 13;
+		switch (op) {
+		case 1:
+			checksum += ++cmap_ii_put(&map, rnd, i-1)->value;
+			break;
+		case 2:
+			if (cmap_size(map) > 0) erased += cmap_ii_erase(&map, rnd);
+			break;
+		case 3:
+			get += (cmap_ii_get(map, rnd) != NULL);
+			break;
+		}
     }
     difference = clock() - before;
-    printf("CMap_ii added: size: %llu, time: %f, check: %llu\n", cmap_size(map), 1.0 * difference / CLOCKS_PER_SEC, checksum);
-    before = clock();
-    fib1 = 0, fib2 = 1;
-    for (size_t i = 0; i < N*2/3; ++i) {
-        cmap_ii_erase(&map, FIBONACCI_NEXT);
-    }
-    difference = clock() - before;
-    printf("CMap_ii removed: size: %llu, time: %f, check: %llu\n", cmap_size(map), 1.0 * difference / CLOCKS_PER_SEC, checksum);
-
-    before = clock();
-    fib1 = 0, fib2 = 1; checksum = 0;
-    for (size_t i = 0; i < N; ++i) {
-        checksum += ++cmap_ii_put(&map, i, i)->value;
-    }
-    difference = clock() - before;
-    printf("CMap_ii re-add: size: %llu, time: %f, check: %llu\n", cmap_size(map), 1.0 * difference / CLOCKS_PER_SEC, checksum);
-    
-    size_t sum = 0;
-    c_foreach (i, cmap_ii, map) sum += cmap_ii_get(map, i.item->key)->value;
-    
+    printf("CMap_ii: size: %llu, time: %f, sum: %llu, erased: %llu, get %llu\n", cmap_size(map), 1.0 * difference / CLOCKS_PER_SEC, checksum, erased, get);
+    //c_foreach (i, cmap_ii, map) sum += cmap_ii_get(map, i.item->key)->value;
     cmap_ii_destroy(&map);
-
 
     std::unordered_map<int, int> map2;
     //map2.reserve(N);
+    srand(123);
     before = clock();
-    fib1 = 0, fib2 = 1; checksum = 0;
-    for (size_t i = 0; i < N; ++i)
-        checksum += ++(map2[FIBONACCI_NEXT] = i);
+    checksum = 0; erased = 0; get = 0;
+    for (size_t i = 0; i < N; ++i) {
+		int rnd = rand();
+		int op = rand() >> 13;
+		switch (op) {		
+		case 1:
+			checksum += ++(map2[rnd] = i-1);
+			break;
+		case 2:
+			if (map2.size() > 0) erased += map2.erase(rnd);
+			break;
+		case 3:
+			get += (map2.find(rnd) != map2.end());
+			break;
+		}
+    }
     difference = clock() - before;
-    printf("std::unordered_map added: size: %llu, time: %f, check: %llu\n", map2.size(), 1.0 * difference / CLOCKS_PER_SEC, checksum);
-    
-    before = clock();
-    fib1 = 0, fib2 = 1; checksum = 0;
-    for (size_t i = 0; i < N*2/3; ++i)
-        map2.erase(FIBONACCI_NEXT);
-    difference = clock() - before;
-    printf("std::unordered_map erased: size: %llu, time: %f\n", map2.size(), 1.0 * difference / CLOCKS_PER_SEC);
-   
-    before = clock();
-    fib1 = 0, fib2 = 1; checksum = 0;
-    for (size_t i = 0; i < N; ++i)
-        checksum += ++(map2[i] = i);
-    difference = clock() - before;
-    printf("std::unordered_map re-add: size: %llu, time: %f, check: %llu\n", map2.size(), 1.0 * difference / CLOCKS_PER_SEC, checksum);
-    
+    printf("std::um: size: %llu, time: %f, sum: %llu, erased: %llu, get %llu\n", cmap_size(map), 1.0 * difference / CLOCKS_PER_SEC, checksum, erased, get);
     map2.clear();
 }
