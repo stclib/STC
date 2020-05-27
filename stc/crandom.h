ab6b3e70352dcba3d5239970a597fa8bb8407b02
Tylo <tylo@norceresearch.no>
2020-05-27 19:32:08 +0200

    Added rngtest.c and included xoroshiro128ss_rand() and sfc64_rand()

--- stc/crandom.h
+++ stc/crandom.h
@@ -52,12 +52,12 @@ enum { /* period parameters */
 };
 
 typedef struct mt19937 {
-    uint32_t idx;
-    uint32_t arr[mt19937_N]; /* the array for the state vector */
+    uint_fast32_t idx;
+    uint_fast32_t arr[mt19937_N]; /* the array for the state vector */
 } mt19937_t;
 
 /* initializes state with a seed */
-static inline void mt19937_init(mt19937_t *state, uint32_t seed) {
+static inline void mt19937_init(mt19937_t *state, uint_fast32_t seed) {
     state->idx = mt19937_N;
     state->arr[0] = seed;
     for (int i = 1; i < mt19937_N; ++i) {
@@ -69,7 +69,7 @@ static inline void mt19937_init(mt19937_t *state, uint32_t seed) {
     }
 }
 /* creates a new state from a seed */
-static inline mt19937_t mt19937_seed(uint32_t seed) {
+static inline mt19937_t mt19937_seed(uint_fast32_t seed) {
     mt19937_t state;
     mt19937_init(&state, seed);
     return state;
@@ -82,10 +82,10 @@ static inline mt19937_t mt19937_default(void) {
 }
 
 /* generates a random number on [0, 0xffffffff]-interval */
-static inline uint32_t mt19937_rand(mt19937_t *state) {
+static inline uint_fast32_t mt19937_rand(mt19937_t *state) {
     enum {N = mt19937_N, M = mt19937_M};
 
-    uint32_t y, *arr = state->arr;
+    uint_fast32_t y, *arr = state->arr;
     if (state->idx >= N) { /* generate N words at one time */
         int k = 0;
         for (; k < N-M; ++k) {
@@ -108,6 +108,66 @@ static inline uint32_t mt19937_rand(mt19937_t *state) {
     y ^= (y << 15) & 0xefc60000;
     y ^= (y >> 18);
     return y;
+}
+
+/* xoroshiro128** and xoshiro128** with splitmix64 seed initialization */
+
+static inline uint64_t splitmix64(uint64_t *state) {
+    uint64_t z = (*state += 0x9e3779b97f4a7c15);
+    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+    z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+    return z ^ (z >> 31);
+}
+
+static inline uint64_t c_rotl(uint64_t x, int s) {
+    return (x << s) | (x >> (64 - s));
+}
+
+/* xoroshiro128** */
+
+typedef struct xoroshiro128ss {
+    uint64_t s[2];
+} xoroshiro128ss_t;
+
+static inline xoroshiro128ss_t xoroshiro128ss_seed(uint64_t seed) {
+    xoroshiro128ss_t state;
+    state.s[0] = splitmix64(&seed);
+    state.s[1] = splitmix64(&seed);
+    return state;
+}
+
+static inline uint64_t xoroshiro128ss_rand(xoroshiro128ss_t *state) {
+    uint64_t *s = state->s, s1 = s[1];
+    const uint64_t s0 = s[0];
+    const uint64_t result = c_rotl(s0 * 5, 7) * 9;
+    s1 ^= s0;
+    s[0] = c_rotl(s0, 24) ^ s1 ^ (s1 << 16);
+    s[1] = c_rotl(s1, 37);
+    return result;
+}
+
+/* http://pracrand.sourceforge.net */
+
+typedef struct sfc64 {
+    uint64_t s[3], counter;
+} sfc64_t;
+
+
+static inline uint64_t sfc64_rand(sfc64_t* state) {
+    enum {LROT = 24, RSHIFT = 11, LSHIFT = 3};
+    uint64_t *s = state->s;
+
+    uint64_t result = s[0] + s[1] + ++state->counter;
+    s[0] = s[1] ^ (s[1] >> RSHIFT);
+    s[1] = s[2] + (s[2] << LSHIFT);
+    s[2] = ((s[2] << LROT) | (s[2] >> (64-LROT))) + result;
+    return result;
+}
+
+static inline sfc64_t sfc64_seed(uint64_t seed) {
+    sfc64_t state = {{seed, seed, seed}, 0};
+    for (int i = 0; i < 12; ++i) sfc64_rand(&state);
+    return state;
 }
 
 #endif
