11db093d6037ed8737cfa4fb7ed44b9e09deef33
tylo <tylo@norceresearch.no>
2020-09-07 10:15:41 +0200

    Added back cmap insert()

--- examples/mapmap.c
+++ examples/mapmap.c
@@ -9,14 +9,14 @@ declare_cmap_strkey(cfg, cmap_str, cmap_str_destroy);
 int main(void) {
     cmap_cfg config = cmap_ini;
     cmap_str init = cmap_ini;
-    cmap_str_insert_or_assign(&cmap_cfg_emplace(&config, "user", init).item->value, "name", "Joe");
-    cmap_str_insert_or_assign(&cmap_cfg_emplace(&config, "user", init).item->value, "groups", "proj1,proj3");
-    cmap_str_insert_or_assign(&cmap_cfg_emplace(&config, "group", init).item->value, "proj1", "Energy");
-    cmap_str_insert_or_assign(&cmap_cfg_emplace(&config, "group", init).item->value, "proj2", "Windy");
-    cmap_str_insert_or_assign(&cmap_cfg_emplace(&config, "group", init).item->value, "proj3", "Oil");
-    cmap_str_insert_or_assign(&cmap_cfg_emplace(&config, "admin", init).item->value, "employees", "2302");
+    cmap_str_put(&cmap_cfg_insert(&config, "user", init).item->value, "name", "Joe");
+    cmap_str_put(&cmap_cfg_insert(&config, "user", init).item->value, "groups", "proj1,proj3");
+    cmap_str_put(&cmap_cfg_insert(&config, "group", init).item->value, "proj1", "Energy");
+    cmap_str_put(&cmap_cfg_insert(&config, "group", init).item->value, "proj2", "Windy");
+    cmap_str_put(&cmap_cfg_insert(&config, "group", init).item->value, "proj3", "Oil");
+    cmap_str_put(&cmap_cfg_insert(&config, "admin", init).item->value, "employees", "2302");
 
-    cmap_str_insert_or_assign(&cmap_cfg_emplace(&config, "group", init).item->value, "proj2", "Wind"); // Update
+    cmap_str_put(&cmap_cfg_insert(&config, "group", init).item->value, "proj2", "Wind"); // Update
 
     c_foreach (i, cmap_cfg, config)
         c_foreach (j, cmap_str, i.item->value)
