103d8d501cf9a3cb3c882e05553e79a5731a0c26
tylo <tylo@norceresearch.no>
2021-09-17 12:07:24 +0200

    Some cleanups in test.

--- tests/test_new_arr.c
+++ tests/test_new_arr.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 
-#define i_tag i
 #define i_val int
 #include <stc/carr2.h>
 
 #define i_val int
 #include <stc/carr3.h>
-#include <stdio.h>
 
 int main()
 {
     int w = 7, h = 5, d = 3;
 
-    c_forvar (carr2_i image = carr2_i_init(w, h), carr2_i_del(&image))
+    c_forvar (carr2_int image = carr2_int_init(w, h), carr2_int_del(&image))
     {
-        int *dat = carr2_i_data(&image);
-        for (int i = 0; i < carr2_i_size(image); ++i)
+        int *dat = carr2_int_data(&image);
+        for (size_t i = 0; i < carr2_int_size(image); ++i)
             dat[i] = i;
 
-        for (int x = 0; x < image.xdim; ++x)
-            for (int y = 0; y < image.ydim; ++y)
+        for (size_t x = 0; x < image.xdim; ++x)
+            for (size_t y = 0; y < image.ydim; ++y)
                 printf(" %d", image.data[x][y]);
         puts("");
 
-        c_foreach (i, carr2_i, image)
+        c_foreach (i, carr2_int, image)
             printf(" %d", *i.ref);
     }
     puts("\n");
@@ -31,12 +29,12 @@ int main()
     c_forvar (carr3_int image = carr3_int_init(w, h, d), carr3_int_del(&image))
     {
         int *dat = carr3_int_data(&image);
-        for (int i = 0; i < carr3_int_size(image); ++i)
+        for (size_t i = 0; i < carr3_int_size(image); ++i)
             dat[i] = i;
 
-        for (int x = 0; x < image.xdim; ++x)
-            for (int y = 0; y < image.ydim; ++y)
-                for (int z = 0; z < image.zdim; ++z)
+        for (size_t x = 0; x < image.xdim; ++x)
+            for (size_t y = 0; y < image.ydim; ++y)
+                for (size_t z = 0; z < image.zdim; ++z)
                     printf(" %d", image.data[x][y][z]);
         puts("");
 
