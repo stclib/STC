174fa5455f4d4f9bb0f59108cb178a8304ddd7e9
Tyge Løvset <tylo@norceresearch.no>
2023-10-06 11:56:02 +0200

    Added (experimental) alternative header names with new default container names (when not specifying i_type) in new include/stx folder. I.e. these *may* replace originals from include/stc in a future release. See example demos.c: - stx/arc.h (Rust Arc/Rc) - stx/box.h (Rust Box) - stx/deq.h - stx/hmap.h (cmap.h) - hash map - stx/hset.h (cset.h) - hash set - stx/list.h - forward list - stx/pque.h - priority queue - stx/queue.h - stx/smap.h - sorted map - stx/sset.h - sorted set - stx/stack.h - stx/vec.h

--- misc/examples/mixed/demos.c
+++ misc/examples/mixed/demos.c
@@ -29,157 +29,155 @@ void stringdemo1(void)
 }
 
 #define i_key long long
-#define i_tag ix
-#include <stc/cvec.h>
+#define i_tag ll
+#include <stx/vec.h>
 
 void vectordemo1(void)
 {
-    cvec_ix bignums = cvec_ix_with_capacity(100);
-    cvec_ix_reserve(&bignums, 100);
+    vec_ll bignums = vec_ll_with_capacity(100);
+    vec_ll_reserve(&bignums, 100);
     for (int i = 10; i <= 100; i += 10)
-        cvec_ix_push(&bignums, i * i);
+        vec_ll_push(&bignums, i * i);
 
     printf("erase - %d: %lld\n", 3, bignums.data[3]);
-    cvec_ix_erase_n(&bignums, 3, 1); // erase index 3
+    vec_ll_erase_n(&bignums, 3, 1); // erase index 3
 
-    cvec_ix_pop(&bignums);           // erase the last
-    cvec_ix_erase_n(&bignums, 0, 1); // erase the first
+    vec_ll_pop(&bignums);           // erase the last
+    vec_ll_erase_n(&bignums, 0, 1); // erase the first
 
-    for (int i = 0; i < cvec_ix_size(&bignums); ++i) {
+    for (int i = 0; i < vec_ll_size(&bignums); ++i) {
         printf("%d: %lld\n", i, bignums.data[i]);
     }
 
-    cvec_ix_drop(&bignums);
+    vec_ll_drop(&bignums);
 }
 
 #define i_key_str
 #define i_use_cmp
-#include <stc/cvec.h>
+#include <stx/vec.h>
 
 void vectordemo2(void)
 {
-    cvec_str names = {0};
-    cvec_str_emplace_back(&names, "Mary");
-    cvec_str_emplace_back(&names, "Joe");
-    cvec_str_emplace_back(&names, "Chris");
+    vec_str names = {0};
+    vec_str_emplace_back(&names, "Mary");
+    vec_str_emplace_back(&names, "Joe");
+    vec_str_emplace_back(&names, "Chris");
     cstr_assign(&names.data[1], "Jane"); // replace Joe
     printf("names[1]: %s\n", cstr_str(&names.data[1]));
 
-    cvec_str_sort(&names);               // Sort the array
+    vec_str_sort(&names);               // Sort the array
     
-    c_foreach (i, cvec_str, names)
+    c_foreach (i, vec_str, names)
         printf("sorted: %s\n", cstr_str(i.ref));
 
-    cvec_str_drop(&names);
+    vec_str_drop(&names);
 }
 
 #define i_key int
-#define i_tag ix
 #define i_use_cmp
-#include <stc/clist.h>
+#include <stx/list.h>
 
 void listdemo1(void)
 {
-    clist_ix nums = {0}, nums2 = {0};
+    list_int nums = {0}, nums2 = {0};
     for (int i = 0; i < 10; ++i)
-        clist_ix_push_back(&nums, i);
+        list_int_push_back(&nums, i);
     for (int i = 100; i < 110; ++i)
-        clist_ix_push_back(&nums2, i);
+        list_int_push_back(&nums2, i);
 
     /* splice nums2 to front of nums */
-    clist_ix_splice(&nums, clist_ix_begin(&nums), &nums2);
-    c_foreach (i, clist_ix, nums)
+    list_int_splice(&nums, list_int_begin(&nums), &nums2);
+    c_foreach (i, list_int, nums)
         printf("spliced: %d\n", *i.ref);
     puts("");
 
-    *clist_ix_find(&nums, 104).ref += 50;
-    clist_ix_remove(&nums, 103);
-    clist_ix_iter it = clist_ix_begin(&nums);
-    clist_ix_erase_range(&nums, clist_ix_advance(it, 5), clist_ix_advance(it, 15));
-    clist_ix_pop_front(&nums);
-    clist_ix_push_back(&nums, -99);
-    clist_ix_sort(&nums);
+    *list_int_find(&nums, 104).ref += 50;
+    list_int_remove(&nums, 103);
+    list_int_iter it = list_int_begin(&nums);
+    list_int_erase_range(&nums, list_int_advance(it, 5), list_int_advance(it, 15));
+    list_int_pop_front(&nums);
+    list_int_push_back(&nums, -99);
+    list_int_sort(&nums);
 
-    c_foreach (i, clist_ix, nums)
+    c_foreach (i, list_int, nums)
         printf("sorted: %d\n", *i.ref);
 
-    c_drop(clist_ix, &nums, &nums2);
+    c_drop(list_int, &nums, &nums2);
 }
 
 #define i_key int
-#define i_tag i
-#include <stc/cset.h>
+#include <stx/hset.h>
 
 void setdemo1(void)
 {
-    cset_i nums = {0};
-    cset_i_insert(&nums, 8);
-    cset_i_insert(&nums, 11);
+    hset_int nums = {0};
+    hset_int_insert(&nums, 8);
+    hset_int_insert(&nums, 11);
 
-    c_foreach (i, cset_i, nums)
+    c_foreach (i, hset_int, nums)
         printf("set: %d\n", *i.ref);
-    cset_i_drop(&nums);
+    hset_int_drop(&nums);
 }
 
 #define i_key int
 #define i_val int
 #define i_tag ii
-#include <stc/cmap.h>
+#include <stx/hmap.h>
 
 void mapdemo1(void)
 {
-    cmap_ii nums = {0};
-    cmap_ii_insert(&nums, 8, 64);
-    cmap_ii_insert(&nums, 11, 121);
-    printf("val 8: %d\n", *cmap_ii_at(&nums, 8));
-    cmap_ii_drop(&nums);
+    hmap_ii nums = {0};
+    hmap_ii_insert(&nums, 8, 64);
+    hmap_ii_insert(&nums, 11, 121);
+    printf("val 8: %d\n", *hmap_ii_at(&nums, 8));
+    hmap_ii_drop(&nums);
 }
 
 #define i_key_str
 #define i_val int
 #define i_tag si
-#include <stc/cmap.h>
+#include <stx/hmap.h>
 
 void mapdemo2(void)
 {
-    cmap_si nums = {0};
-    cmap_si_emplace_or_assign(&nums, "Hello", 64);
-    cmap_si_emplace_or_assign(&nums, "Groovy", 121);
-    cmap_si_emplace_or_assign(&nums, "Groovy", 200); // overwrite previous
+    hmap_si nums = {0};
+    hmap_si_emplace_or_assign(&nums, "Hello", 64);
+    hmap_si_emplace_or_assign(&nums, "Groovy", 121);
+    hmap_si_emplace_or_assign(&nums, "Groovy", 200); // overwrite previous
 
     // iterate the map:
-    for (cmap_si_iter i = cmap_si_begin(&nums); i.ref; cmap_si_next(&i))
+    for (hmap_si_iter i = hmap_si_begin(&nums); i.ref; hmap_si_next(&i))
         printf("long: %s: %d\n", cstr_str(&i.ref->first), i.ref->second);
 
     // or rather use the short form:
-    c_foreach (i, cmap_si, nums)
+    c_foreach (i, hmap_si, nums)
         printf("short: %s: %d\n", cstr_str(&i.ref->first), i.ref->second);
 
-    cmap_si_drop(&nums);
+    hmap_si_drop(&nums);
 }
 
 #define i_key_str
 #define i_val_str
-#include <stc/cmap.h>
+#include <stx/hmap.h>
 
 void mapdemo3(void)
 {
-    cmap_str table = {0};
-    cmap_str_emplace(&table, "Map", "test");
-    cmap_str_emplace(&table, "Make", "my");
-    cmap_str_emplace(&table, "Sunny", "day");
-    cmap_str_iter it = cmap_str_find(&table, "Make");
-    c_foreach (i, cmap_str, table)
+    hmap_str table = {0};
+    hmap_str_emplace(&table, "Map", "test");
+    hmap_str_emplace(&table, "Make", "my");
+    hmap_str_emplace(&table, "Sunny", "day");
+    hmap_str_iter it = hmap_str_find(&table, "Make");
+    c_foreach (i, hmap_str, table)
         printf("entry: %s: %s\n", cstr_str(&i.ref->first), cstr_str(&i.ref->second));
-    printf("size %" c_ZI ": remove: Make: %s\n", cmap_str_size(&table), cstr_str(&it.ref->second));
-    //cmap_str_erase(&table, "Make");
-    cmap_str_erase_at(&table, it);
+    printf("size %" c_ZI ": remove: Make: %s\n", hmap_str_size(&table), cstr_str(&it.ref->second));
+    //hmap_str_erase(&table, "Make");
+    hmap_str_erase_at(&table, it);
 
-    printf("size %" c_ZI "\n", cmap_str_size(&table));
-    c_foreach (i, cmap_str, table)
+    printf("size %" c_ZI "\n", hmap_str_size(&table));
+    c_foreach (i, hmap_str, table)
         printf("entry: %s: %s\n", cstr_str(&i.ref->first), cstr_str(&i.ref->second));
     
-    cmap_str_drop(&table); // frees key and value cstrs, and hash table.
+    hmap_str_drop(&table); // frees key and value cstrs, and hash table.
 }
 
 int main(void)
