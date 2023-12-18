83253bce5a984076141d0b261ea3931c97b3152b
tylov <tylo@norceresearch.no>
2023-12-18 21:39:42 +0100

    c11/strings.h: Renamed generic constructors to cstr_make(...), csview_make(...), czview_make(...)

--- include/c11/strings.h
+++ include/c11/strings.h
@@ -7,29 +7,32 @@
 
 // NB! C11 and later only!
 
-#define cstr(...) c_MACRO_OVERLOAD(cstr, __VA_ARGS__)
-#define cstr_1(a) _Generic((a), \
-    cstr: cstr_clone, \
+#define cstr_make(...) c_MACRO_OVERLOAD(cstr_make, __VA_ARGS__)
+#define cstr_make_1(a) _Generic((a), \
     char*: cstr_from, \
     const char*: cstr_from, \
     csview: cstr_from_sv, \
     czview: cstr_from_zv)(a)
-#define cstr_2(str, n) cstr_from_n(str, n)
-#define cstr_3(sp, pos, n) cstr_from_pos(sp, pos, n)
+#define cstr_make_2(str, n) \
+    cstr_from_n(str, n)
+#define cstr_make_3(s, pos, n) \
+    cstr_from_pos(s, pos, n)
 
-
-#define csview(...) c_MACRO_OVERLOAD(csview, __VA_ARGS__)
-#define csview_1(a) _Generic((a), \
+#define csview_make(...) c_MACRO_OVERLOAD(csview_make, __VA_ARGS__)
+#define csview_make_1(a) _Generic((a), \
     cstr*: cstr_sv, \
     const cstr*: cstr_sv, \
     char*: csview_from, \
     const char*: csview_from, \
     czview: czview_sv)(a)
-#define csview_2(str, n) csview_from_n(str, n)
-#define csview_3(sv, pos, n) csview_substr(sv, pos, n)
+#define csview_make_2(str, n) \
+    csview_from_n(str, n)
+#define csview_make_3(a, pos, n) _Generic((a), \
+    csview: csview_substr, \
+    cstr*: cstr_substr, \
+    const cstr*: cstr_substr)(a, pos, n)
 
-
-#define czview(a) _Generic((a), \
+#define czview_make(a) _Generic((a), \
     cstr*: cstr_zv, \
     const cstr*: cstr_zv, \
     char*: czview_from, \
