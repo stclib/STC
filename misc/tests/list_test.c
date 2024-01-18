22e740124fda8a78b9000dff3d815f685eada334
Tyge Løvset <tylo@norceresearch.no>
2024-01-18 18:30:14 +0100

    Added a few list tests.

--- misc/tests/list_test.c
+++ misc/tests/list_test.c
@@ -16,18 +16,38 @@ TEST(list, splice)
 
     IList res1 = c_init(IList, {1, 2, 10, 20, 30, 40, 50, 3, 4, 5});
     EXPECT_EQ(*pos.ref, 3);
-    EXPECT_TRUE(IList_eq(&list1, &res1));
+    EXPECT_TRUE(IList_eq(&res1, &list1));
     EXPECT_TRUE(IList_is_empty(&list2));
 
     // splice items from pos to end of list1 into empty list2:
     IList_splice_range(&list2, IList_begin(&list2), &list1, pos, IList_end(&list1));
 
     IList res2 = c_init(IList, {1, 2, 10, 20, 30, 40, 50});
-    EXPECT_TRUE(IList_eq(&list1, &res2));
+    EXPECT_TRUE(IList_eq(&res2, &list1));
 
     IList res3 = c_init(IList, {3, 4, 5});
-    EXPECT_TRUE(IList_eq(&list2, &res3));
-
+    EXPECT_TRUE(IList_eq(&res3, &list2));
     EXPECT_FALSE(IList_eq(&list1, &list2));
+
     c_drop(IList, &list1, &list2, &res1, &res2, &res3);
+}
+
+TEST(list, erase)
+{
+    IList L = c_init(IList, {10, 20, 30, 40, 50});
+
+    IList_iter it = IList_begin(&L);
+    IList_next(&it);
+    it = IList_erase_at(&L, it);
+
+    IList res1 = c_init(IList, {10, 30, 40, 50});
+    EXPECT_TRUE(IList_eq(&res1, &L));
+
+    IList_next(&it);
+    it = IList_erase_range(&L, it, IList_end(&L));
+
+    IList res2 = c_init(IList, {10, 30});
+    EXPECT_TRUE(IList_eq(&res2, &L));
+
+    c_drop(IList, &L, &res1, &res2);
 }
--- misc/tests/run.sh
+++ misc/tests/run.sh
@@ -1,16 +1,13 @@
-# run tests
+#!/usr/bin/sh
 # tcc compiler: git://repo.or.cz/tinycc.git
+# run tests
 
 cd $(dirname $(realpath $0))
 
 if [ -z "$1" ]; then
-    cc=gcc
+    tcc -Wall -I../../include *_test.c main.c -run
 else
     cc=$1
-fi
-if [ "$cc" == "tcc" ]; then
-    tcc -O2 -Wall -I../../include *_test.c main.c -run
-else
     $cc -O2 -Wall -I../../include *_test.c main.c -o tests.exe
     ./tests.exe
     rm -f ./tests.exe
