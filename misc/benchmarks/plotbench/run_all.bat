f11a82e4f4049d03c370f0cd1e56ff40076ee717
tylov <tylo@norceresearch.no>
2024-12-30 23:02:38 +0100

    Moved benchmarks to separate repos: stcbenchmark

--- misc/benchmarks/plotbench/run_all.bat
+++
@@ -1,5 +0,0 @@
-@set out=plot_win.csv
-echo Compiler,Library,C,Method,Seconds,Ratio> %out%
-sh run_gcc.sh >> %out%
-sh run_clang.sh >> %out%
-call run_vc.bat >> %out%
--- misc/benchmarks/plotbench/run_all.sh
+++
@@ -1,4 +0,0 @@
-out="plot_linux.csv"
-echo "Compiler,Library,C,Method,Seconds,Ratio"> $out
-sh run_gcc.sh >> $out
-sh run_clang.sh >> $out
